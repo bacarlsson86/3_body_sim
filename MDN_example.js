724f6c3cdcb64ebc8f30f6e33a10bcc57f0da6f6
bacarlsson86 <bacarlsson@gmail.com>
2024-08-06 16:42:04 -0700

    writing notes on MDN animation example

--- MDN_example.js
+++ MDN_example.js
@@ -4,14 +4,23 @@ function MDN_Example(){
     let zero;
     requestAnimationFrame(firstFrame);
     function firstFrame(timeStamp) {
-    zero = timeStamp;
-    animate(timeStamp);
+        zero = timeStamp;
+        animate(timeStamp);
     }
     function animate(timeStamp) {
-    const value = (timeStamp - zero) / duration;
-    if (value < 1) {
-        element.style.opacity = value;
-        requestAnimationFrame((t) => animate(t));
-    } else element.style.opacity = 1;
+        // this function will need access to state so it can
+        // update it
+        // here we will update our time
+        const value = (timeStamp - zero) / duration;
+
+        // each loop will run one simulation step on the state
+        // of the previous frame. we should rewrite our sim
+        // function with an argument of the current state
+        // and a sim_interval set at the outset
+
+        if (value < 1) {
+            element.style.opacity = value;
+            requestAnimationFrame((t) => animate(t));
+        } else element.style.opacity = 1;
     }
 }
