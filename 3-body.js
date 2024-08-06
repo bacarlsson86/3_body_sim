ed7635876a798ef699e0973aefe7a2276fb8c3f7
bacarlsson86 <bacarlsson@gmail.com>
2024-08-05 17:26:53 -0700

    getting point1 and point2 moving!

--- 3-body.js
+++ 3-body.js
@@ -4,8 +4,9 @@
 
 const DEBUG = true;
 if(!DEBUG){
-    if(!window.console) window.console = {}
-    const methods = ["log", "debug", "warn", "info", "dir", "trace"]
+    // if(!window.console) window.console = {}
+    console.log('Supressing console...')
+    const methods = [ "log", "debug", "warn", "info", "dir", "trace"]
     for(let i = 0; i<methods.length; i++){
         console[methods[i]] = function(){}
     }
@@ -26,7 +27,7 @@ if(!DEBUG){
 
 // set our simulation time interval
 const sim_interval = 1;
-const sim_length = 10;
+const sim_length = 100;
 const time = 0;
 const G = 1;
 
@@ -34,10 +35,10 @@ const G = 1;
 
 let point1 = {
     "name" : "point1",
-    "position_x": 1,
-    "position_y": 1,
-    "velocity_x": 0,
-    "velocity_y": 0, 
+    "position_x": 0,
+    "position_y": 0,
+    "velocity_x": -1,
+    "velocity_y": -2, 
     "acceleration_x": 0, 
     "acceleration_y": 0,
     "mass": 1
@@ -45,7 +46,7 @@ let point1 = {
 
 let point2 = {
     "name" : "point2",
-    "position_x": 2,
+    "position_x": 7,
     "position_y": 2,
     "velocity_x": 0,
     "velocity_y": 0, 
@@ -74,40 +75,58 @@ console.table(state);
 function get_distance(a, b){
     // takes two points and calculates the magnitude of the distance
     // between them (pythagorean theorem)
-    console.log(`running get_distance on ${a.name} to ${b.name}`)
-    return Math.sqrt((b.position_x - a.position_x)**2, (b.position_y - a.position_y)**2)
+    // console.log(`running get_distance on ${a.name} to ${b.name}`)
+    return Math.sqrt((b.position_x - a.position_x)**2 + (b.position_y - a.position_y)**2)
 }
 
 function get_direction_vector(a, b){
     // this derives the unit vector pointing from a to b
-    console.log(`getting direction vector from ${a.name} to ${b.name}`);
+    // console.log(`getting direction vector from ${a.name} to ${b.name}`);
     const magnitude = get_distance(a, b);
     const direction_vector = {
-        "x" : (b.position_x - a.position_x) / magnitude,
-        "y" : (b.position_y - a.position_y) / magnitude,
+        "origin": a.name,
+        "destination" : b.name,
+        "x_direction" : (b.position_x - a.position_x) / magnitude,
+        "y_direction" : (b.position_y - a.position_y) / magnitude,
         "magnitude" : magnitude
     };
     return direction_vector;
+}
+
+function create_reversed_vector(a){
+    // reverses vector, duh
+    // console.log('creating reversed vector...');
+    const reversed_vector = {
+        "origin": a.destination,
+        "destination" : a.origin,
+        "x_direction" : -a.x_direction,
+        "y_direction" : -a.y_direction,
+        "magnitude" : a.magnitude
+    };
+    return reversed_vector;
 }
 
 function update_position(a){
     // update the position of a point using its current velocity over
     // one sim interval of time
     console.log(`updating position of ${a.name}`);
-    a.position_x = a.position_x + (velocity_x * sim_interval);
-    a.position_y = a.position_y + (velocity_y * sim_interval);
+    a.position_x = a.position_x + (a.velocity_x * sim_interval);
+    a.position_y = a.position_y + (a.velocity_y * sim_interval);
 
     return a;
 }
 
 function update_velocity(a){
     console.log(`updating velocity of ${a.name}`);
-    a.velocity_x = a.velocity_x + (acceleration_x * sim_interval);
-    a.velocity_y = a.velocity_y + (acceleration_y * sim_interval);
+    a.velocity_x = a.velocity_x + (a.acceleration_x * sim_interval);
+    a.velocity_y = a.velocity_y + (a.acceleration_y * sim_interval);
 }
 
 function update_accelerations(a, b, c){
-
+    
+    console.log("Inputs");
+    console.dir(arguments);
+    console.log("Updating...")
     // we have to derive each force as a vector. using our
     // and then
 
@@ -118,61 +137,45 @@ function update_accelerations(a, b, c){
     const vect_a_to_c = get_direction_vector(a, c);
     const vect_b_to_c = get_direction_vector(b, c);
 
-    const direct_vectors = {
-            "vect_a_to_b": vect_a_to_b, 
-            "vect_a_to_c": vect_a_to_c, 
-            "vect_b_to_c": vect_b_to_c
-        };
+    // get reverse vectors
+    const vect_b_to_a = create_reversed_vector(vect_a_to_b);
+    const vect_c_to_a = create_reversed_vector(vect_a_to_c);
+    const vect_c_to_b = create_reversed_vector(vect_b_to_c);
+
+    const vectors = [vect_a_to_b, vect_a_to_c, vect_b_to_c, vect_c_to_a, vect_c_to_b, vect_b_to_a]
 
     // get |r|^2
     const rsquared_a_to_b = vect_a_to_b.magnitude**2;
     const rsquared_a_to_c = vect_a_to_c.magnitude**2;
     const rsquared_b_to_c = vect_b_to_c.magnitude**2;
 
-    const rsquareds = {
-            "rsquared_a_to_b": rsquared_a_to_b, 
-            "rsquared_a_to_c": rsquared_a_to_c,
-            "rsquared_b_to_c": rsquared_b_to_c
-        };
-
     // put it all together
-    const force_magnitude_a_and_b = (G *(a.mass * b.mass)) / rsquared_a_to_b;
-    const force_magnitude_a_and_c = (G *(a.mass * b.mass)) / rsquared_a_to_c;
-    const force_magnitude_b_and_c = (G *(a.mass * c.mass)) / rsquared_b_to_c;
-
-    const force_magnitudes = {
-            "force_magnitude_a_and_b": force_magnitude_a_and_b,
-            "force_magnitude_a_and_c": force_magnitude_a_and_c,
-            "force_magnitude_b_and_c": force_magnitude_b_and_c
-            };
 
     // get the components of each force using the unit vector and combine them
-    // a.acceleration_x = force_magnitude_a_and_b * vect_a_to_b.x
-    //                  + force_magnitude_a_and_c * vect_a_to_c.x;
-    
-    // a.acceleration_y = force_magnitude_a_and_b * vect_a_to_b.y
-    //                  + force_magnitude_a_and_c * vect_a_to_c.y;
+    a.acceleration_x = ((G * a.mass * b.mass * vect_a_to_b.x_direction) / rsquared_a_to_b**2)
+    a.acceleration_y = ((G * a.mass * b.mass * vect_a_to_b.y_direction) / rsquared_a_to_b**2)
+    b.acceleration_x = ((G * a.mass * b.mass * vect_b_to_a.x_direction) / rsquared_a_to_b**2)
+    b.acceleration_y = ((G * a.mass * b.mass * vect_b_to_a.y_direction) / rsquared_a_to_b**2)
 
-    console.dir(direct_vectors);
-    console.dir(rsquareds);
-    console.dir(force_magnitudes);
-    // const force_a_and_b = 0;
-    // const force_a_and_c = 0;
-    // const force_b_and_c = 0;
-
-    // the convention is that the force vector of the first
-    // point in the variable name is the "positive" direction
-
-    // a.acceleration = (force_a_and_b + force_a_and_c) / a.mass
-    // b.acceleration = -force_a_and_b + force_b_and_c / b.mass
-    // c.acceleration = -force_a_and_c + -force_b_and_c / c.mass
+    // console.log("Outputs:")
+    // console.dir(vectors);
 }
 
-function update_state(state){
+function run_sim(state){
+    for(let i=0;i<=sim_length;i++){
+        console.log(`Loop number ${i}`)
+        update_accelerations(...state);
+        update_position(state[0]);
+        update_position(state[1]);
+        update_velocity(state[0]);
+        update_velocity(state[1]);
+    }
+    console.log("Simulation complete")
     // loop over sim time
         // update_position on each point
         // update_acceleration on each point
         // update_velocity on each point
 }
 
-update_accelerations(...state);
+console.table(state)
+run_sim(state);
